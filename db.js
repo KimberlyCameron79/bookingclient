5dc9b4eb4975d54a23cdabdab28873d4db240773
KimberlyCameron79 <cameronkd7@live.com>
2022-11-08 10:54:01 -0500

    capstone

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const mongoURL ='mongodb+srv://kimberlycam43:Kc0370%24%24@cluster0.xbzyqi5.mongodb.net/test'
+
+mongoose.connect(mongoURL, {useUnifiedTopology : true , useNewUrlParser:true})
+
+const connection = mongoose.connection
+
+connection.on('error', () => {
+    console.log('Mongo DB Connection failed')
+})
+
+connection.on('connected' , () => {
+    console.log('Mongo DB Connection Successful')
+})
+
+
+module.exports = mongoose
