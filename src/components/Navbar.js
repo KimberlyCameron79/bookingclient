399261406f9d1d9423d5c48dd2fef9df9f83ab19
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-11-09 16:03:01 -0500

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg">
+            <nav class="navbar navbar-expand-lg ">
                 <a class="navbar-brand" href="#">KDC AirBnB</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -11,8 +11,8 @@ function Navbar() {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item active">
-                            <a class="nav-link" href="/register">
-                                Register
+                            <a class="nav-link" href="register">
+                                Register 
                             </a>
                         </li>
                         <li class="nav-item">
@@ -20,6 +20,7 @@ function Navbar() {
                                 Login
                             </a>
                         </li>
+                                              
                     </ul>
                 </div>
             </nav>
