46e8ff2e8d6f005fca7e458e02b4e9ea7f6ddf59
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-11-09 16:02:41 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar';
+
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+// import roomsRoute from '../../routes/roomsRoute'
+import Home from './screens/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+        
+      </Routes>
+      </BrowserRouter>
+              
     </div>
   );
 }
