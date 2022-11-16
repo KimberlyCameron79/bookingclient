6c4c9df381874ae0af418310d501e0246682eb05
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-11-15 20:02:52 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
-// import roomsRoute from '../../routes/roomsRoute'
 import Home from './screens/Home';
-
+import Booking from './screens/Booking';
+import Register from './screens/Register';
+import Login from './screens/Login';
+import Profile from './screens/Profile';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,10 @@ function App() {
       <BrowserRouter>
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/book/:roomid/:fromdate/:todate" element={<Booking />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />}/>
+      <Route path="/profile" element={<Profile />}/>
         
       </Routes>
       </BrowserRouter>
