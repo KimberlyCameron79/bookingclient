119c9a5b83f3d5be408ca052b62d6d9db51b42ee
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-11-16 23:29:25 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -9,13 +9,33 @@ const bookingsRoute = require('./routes/bookingsRoute')
 app.use(cors())
 app.use(express.json())
 
+
+app.post("/auth/login", async (req, res) => {
+    const { email, password } = req.body
+    console.log('running')
+
+    try {
+        const user = await User.findOne({ email: email, password: password })
+        if (user) {
+            res.send(user)
+        }
+        else {
+            return res.status(400).json({ message: "Login Failed" });
+        }
+    } catch (error) {
+        return res.status(400).json({ error });
+
+    }
+}
+
+);
 //creates access to routes
 app.use('/api/rooms', roomsRoute)
 app.use('/api/users', usersRoute)
 app.use('/api/bookings', bookingsRoute)
 
 
-const port= process.env.PORT || 5000;
+const port = 8080;
 
 // dbConfig();
 
